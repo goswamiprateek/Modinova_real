67d930da33440dcce1390363945214ddc9352fa2
prateek kumar goswami <91801572+goswamiprateek@users.noreply.github.com>
2023-09-17 13:59:18 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using Medinova.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Medinova.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Service()
+        {
+            return View();
+        }
+        public IActionResult Pricing()
+        {
+            return View();
+        }
+       public IActionResult Blog()
+        {
+            return View();
+        }
+        public IActionResult Team()
+        {
+            return View();
+        }
+        
+        public IActionResult testimonial()
+        {
+            return View();
+        }
+        public IActionResult Appointment()
+        {
+            return View();
+
+        }
+
+        public IActionResult Search()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+       
+        
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
